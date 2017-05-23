d51a1fb4784f64070912eabe9a91751ddcc7fb7d
Alex <ajshort2010@hotmail.com>
2017-05-23 14:19:46 -0700

    No longer have an issue with overflow.

--- src/server.py
+++ src/server.py
@@ -2,13 +2,24 @@
 
 import sys
 import socket
+import math
+
+
+def response_ok():
+    """."""
+    return '200 OK'
+
+
+def response_error():
+    """."""
+    return '500 Internal Server Error'
+
 
 try:
-
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM,
                            socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 5001)
+    address = ('127.0.0.1', 5000)
     server.bind(address)
     print('Echo server is running.')
     while True:
@@ -17,13 +28,18 @@ try:
         buffer_length = 8
         message_complete = False
         message = ''
-        while not message_complete:
+        flag = 1
+        i = 0
+        while flag == 1:
+            iterations = math.ceil(int(conn.recv(buffer_length).decode('utf8')) / buffer_length)
+            conn.send('ok'.encode('utf8'))
+            flag = 0
+        for i in range(iterations):
             part = conn.recv(buffer_length)
-            message = message + part.decode('utf8')
-            if message == '':
-                break
-            conn.sendall(message.encode('utf8'))
-            message = ''
+            message += part.decode('utf8')
+        conn.sendall(message.encode('utf8'))
+        message = ''
+        conn.send(response_ok().encode('utf8'))
         conn.close()
 
 except KeyboardInterrupt:
