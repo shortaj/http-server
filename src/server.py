6e9f86dd0785b04bbd14a4f195e4f3ef036d0111
Alex <ajshort2010@hotmail.com>
2017-05-23 12:27:14 -0700

    Added KeyboardInterrupt Exception

--- src/server.py
+++ src/server.py
@@ -1,20 +1,32 @@
+"""Server creation for echo assignment."""
+
+import sys
 import socket
-server = socket.socket(socket.AF_INET,
-                       socket.SOCK_STREAM,
-                       socket.IPPROTO_TCP)
-address = ('127.0.0.1',5001)
-server.bind(address)
-while True:
-    server.listen(1)
-    conn,addr = server.accept()
-    buffer_length = 8
-    message_complete = False
-    message = ''
-    while not message_complete:
-        part = conn.recv(buffer_length)
-        message = message + part.decode('utf8')
-        if message == '':
-            break
-        conn.sendall(message.encode('utf8'))
+
+try:
+
+    server = socket.socket(socket.AF_INET,
+                           socket.SOCK_STREAM,
+                           socket.IPPROTO_TCP)
+    address = ('127.0.0.1', 5001)
+    server.bind(address)
+    print('Echo server is running.')
+    while True:
+        server.listen(1)
+        conn, addr = server.accept()
+        buffer_length = 8
+        message_complete = False
         message = ''
-    conn.close()
+        while not message_complete:
+            part = conn.recv(buffer_length)
+            message = message + part.decode('utf8')
+            if message == '':
+                break
+            conn.sendall(message.encode('utf8'))
+            message = ''
+        conn.close()
+
+except KeyboardInterrupt:
+    server.close()
+    print('Echo server is closed.')
+    sys.exit()
