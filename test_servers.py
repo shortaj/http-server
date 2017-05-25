a6433505723bb019e44ebc3ee484056bf98c6306
Alex <ajshort2010@hotmail.com>
2017-05-25 14:34:33 -0700

    Implement test functions for the step2 branch.

--- test_servers.py
+++ test_servers.py
@@ -1,11 +1,40 @@
-"""Tests for client if they receive modified text."""
+"""Tests for client/server if they receive/send proper response."""
 import pytest
 
-MESSAGE = ['Goodbye', 'Hello', 'moNkey', '1234']
+
+METHOD = ['GET', 'POST', 'DELETE', 'PUT', ' ']
+
+PATH = ['/src/server.py', 'invalid path']
+
+VERSION = ['HTTP/1.1', 'HTTP/1.0']
 
 
-@pytest.mark.parametrize('message', MESSAGE)
-def test_talk(message):
-    """Test that the server sends back the client message."""
+@pytest.fixture
+def client_to_server_talk(method, path, version, post_header):
+    """Import the talk function from client and fill it."""
     from client import talk
-    assert talk(message) == message
+    request = method + ' ' + path + version + post_header
+    return talk(request)
+
+
+def test_proper_request():
+    """Test a proper request."""
+    assert client_to_server_talk(METHOD[0], PATH[0], VERSION[0], '\r\n\r\n') == 'HTTP/1.1 200 OK'
+
+
+def test_method():
+    """Test that the server returns the proper Error for bad methods and no method in request."""
+    assert client_to_server_talk(METHOD[1], PATH[0], VERSION[0], '\r\n\r\n') == 'HTTP/1.1 405 Method Not Allowed'
+    assert client_to_server_talk(METHOD[2], PATH[0], VERSION[0], '\r\n\r\n') == 'HTTP/1.1 405 Method Not Allowed'
+    assert client_to_server_talk(METHOD[3], PATH[0], VERSION[0], '\r\n\r\n') == 'HTTP/1.1 405 Method Not Allowed'
+    assert client_to_server_talk(METHOD[4], PATH[0], VERSION[0], '\r\n\r\n') == 'HTTP/1.1 405 Method Not Allowed'
+
+
+def test_path():
+    """Test that the server returns the proper Error for invalid path."""
+    assert client_to_server_talk(METHOD[0], PATH[1], VERSION[0], '\r\n\r\n') == ''
+
+
+def test_version():
+    """Test that the server returns the proper Error for bad version."""
+    assert client_to_server_talk(METHOD[0], PATH[0], VERSION[1], '\r\n\r\n') == ''
