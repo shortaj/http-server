5e69417594fce178db7f031c467d646fd1fe3267
Alex <ajshort2010@hotmail.com>
2017-05-29 13:25:11 -0700

    some of the functionality has been added for concurrency.

--- src/server.py
+++ src/server.py
@@ -2,42 +2,44 @@
 
 import sys
 import socket
-import math
 import os
+import select
+
 
 def file_open_close(path):
-    """Open, read, and close the file a tthe given path"""
+    """Open, read, and close the file a tthe given path."""
     file_info = ''
     req_file = ''
     if '.jpg' in path or '.png' in path:
         print('hi')
-        req_file = open(path,'rb')
+        req_file = open(path, 'rb')
         file_info = req_file.read()
     else:
-        req_file = open(path,'r')
+        req_file = open(path, 'r')
         file_info = req_file.read().encode('utf-8')
     req_file.close()
     return file_info
 
+
 def resolve_uri(message):
-    path=message[message.index('/'):message.index('HTTP')]
-    path = path[:len(path)-1]
-    #path = os.path.join(os.getcwd(), path)
-    #print(os.getcwd())
-    path = os.getcwd() +path
+    """."""
+    path = message[message.index('/'):message.index('HTTP')]
+    path = path[:len(path) - 1]
+    path = os.getcwd() + path
     real = os.path.lexists(path)
     if real:
         return path
     return False
 
-def response_ok(new_file = b''):
-    """Response will now geive return 200 code plus body"""
+
+def response_ok(new_file=b''):
+    """Response will now geive return 200 code plus body."""
     reply = b'HTTP/1.1 200 OK\r\n\r\n'
     response = reply + new_file
     return response
 
 
-def response_error(x = 500):
+def response_error(x=500):
     """Response error return the proper Error Codes."""
     if x == 500:
         return b'HTTP/1.1 500 Internal Server Error\r\n\r\n'
@@ -50,22 +52,22 @@ def response_error(x = 500):
     elif x == 400:
         return b'HTTP/1.1 400 Bad Request\r\n\r\n'
 
+
 def parse_request(message):
-    """Parse request takes in the incoming message for the client"""
+    """Parse request takes in the incoming message for the client."""
     print(message)
     if message[:4] == 'GET 'or message[:7] == 'DELETE ' or message[:5] == 'POST 'or message[:4] == 'PUT ':
         if message[:4] != 'GET ':
             return response_error(405)
     else:
         return response_error(400)
-    if 'HTTP/1.1' not in message:# or not (message[message.index('HTTP'):message.index('HTTP') + 11].endswith('\r\n ') or message[message.index('HTTP'):message.index('HTTP') + 13].endswith('\\r\\n ')):
+    if 'HTTP/1.1' not in message:
         return response_error(505)
-    if 'Host' not in message or not ( message.endswith('\r\n\r\n') or  message.endswith('\\r\\n\\r\\n')):
+    if 'Host' not in message or not (message.endswith('\r\n\r\n') or message.endswith('\\r\\n\\r\\n')):
         if message.endswith('\r\n\r\n'):
             print('I am here')
         return response_error(400)
     return response_ok()
-    
 
 
 try:
@@ -74,42 +76,47 @@ try:
                            socket.IPPROTO_TCP)
     address = ('127.0.0.1', 5001)
     server.bind(address)
+    server.listen(1)
     print('Server is running.')
+    channels = [server, sys.stdin]
+    buffer_length = 8
     while True:
-        server.listen(1)
-        conn, addr = server.accept()
-        buffer_length = 8
-        message_complete = False
-        message = ''
-        flag = 1
-        i = 0
-        current_message_len = 0
-        pro_message_len = buffer_length
-        bitmessage = b''
-        while  not message.endswith('\r\n\r\n') or not part.endswith(b'\r\n\r\n'):
-            #print(1)
-            flag = 0
-            part = conn.recv(buffer_length)
-            bitmessage += part
-            message += part.decode('utf8')
-            current_message_len = len(message)
-            if current_message_len != pro_message_len:
+        read_ready, write_ready, except_ready = select.select(channels, [], [], 0)
+        for readable in read_ready:
+            if readable is server:
+                conn, addr = server.accept()
+                channels.append(handler_socket)
+            elif readable is sys.stdin:
+                sys.stdin.readline()
                 break
             else:
-                pro_message_len += buffer_length
-        print(2)
-        t = parse_request(message)
-        print(3)
-        if b'200' in t:
-            path = resolve_uri(message)
-            print(path)
-            if path:
-                conn.send(response_ok(file_open_close(path)))
-            else:
-                conn.send(parse_request(message))
-        conn.close()
-        #conn.send(response_ok())
-
+                message_complete = False
+                message = ''
+                i = 0
+                current_message_len = 0
+                pro_message_len = buffer_length
+                bitmessage = b''
+                while not message.endswith('\r\n\r\n') or not part.endswith(b'\r\n\r\n'):
+                    part = conn.recv(buffer_length)
+                    bitmessage += part
+                    message += part.decode('utf8')
+                    current_message_len = len(message)
+                    if current_message_len != pro_message_len:
+                        break
+                    else:
+                        pro_message_len += buffer_length
+                print(2)
+                t = parse_request(message)
+                print(3)
+                if b'200' in t:
+                    path = resolve_uri(message)
+                    print(path)
+                    if path:
+                        conn.send(response_ok(file_open_close(path)))
+                    else:
+                        conn.send(parse_request(message))
+                conn.close()
+                channels.remove(readable)
 except KeyboardInterrupt:
     server.close()
     print('Server is closed.')
