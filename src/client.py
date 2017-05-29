3f75b36c4c092352cf591e30f252d675e77252ab
Sean Beseler <seanwbeseler@gmail.com>
2017-05-29 11:17:01 -0700

    Add files via upload
    server update

--- src/client.py
+++ src/client.py
@@ -5,26 +5,22 @@ import socket
 
 def talk(message):
     """."""
-    print(message)
     infos = socket.getaddrinfo('127.0.0.1', 5001)
     len(infos)
     buffer_length = 8
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
-    client.send(str(len(message)).encode('utf8'))
-    if client.recv(buffer_length).decode('utf8') == 'ok':
-        client.sendall(message.encode('utf8'))
-        reply = False
-        mess = ''
-        while not reply:
-            part = client.recv(buffer_length)
-            mess = mess + part.decode('utf8')
-            if mess.endswith('\r\n\r\n'):
-                #print(mess[:len(mess) - 6])
-                break
-        client.close()
-        return mess
+    client.sendall(message.encode('utf8'))
+    reply = False
+    mess = ''
+    while not reply:
+        part = client.recv(buffer_length)
+        mess = mess + part.decode('utf8')
+        if mess.endswith('\r\n\r\n'):
+            break
+    client.close()
+    return mess
 
 
 if __name__ == '__main__':
--- src/server.py
+++ src/server.py
@@ -1,52 +1,59 @@
 """Server creation for echo assignment."""
 
-import os
 import sys
 import socket
 import math
+import os
 
-
-def file_open_read_close(path):
-    """Open, read, encode, and close the file at the given path."""
-    req_file = os.open(path, os.O_RDWR)
-    file_info = os.read(req_file).encode('utf-8')
+def file_open_close(path):
+    """Open, read, and close the file a tthe given path"""
+    req_file = os.open(path,os.O_RDWR)
+    file_info = os.read(req_file).encode('uft-8')
     os.close(req_file)
     return file_info
 
+def resolve_uri(message):
+    path=message[message.index('/'):message.index('HTTP')]
+    real = os.path.exists(path)
+    if real:
+        return path
+    return real
 
-def response_ok(file=''):
-    """Response will now give return 200 code plus body."""
+def response_ok(new_file = b''):
+    """Response will now geive return 200 code plus body"""
     reply = b'HTTP/1.1 200 OK\r\n\r\n'
-    response = reply + file
-    return conn.sendall(response)
+    response = reply + new_file
+    return response
 
 
-def response_error(x=500):
-    """Response error will return the proper Error Codes."""
+def response_error(x = 500):
+    """Response error return the proper Error Codes."""
     if x == 500:
         return b'HTTP/1.1 500 Internal Server Error\r\n\r\n'
     elif x == 405:
         return b'HTTP/1.1 405 Method Not Allowed\r\n\r\n'
     elif x == 505:
         return b'HTTP/1.1 505 HTTP Version Not Supported\r\n\r\n'
+    elif x == 404:
+        return b'HTTP/1.1 404 Not Found'
     elif x == 400:
         return b'HTTP/1.1 400 Bad Request\r\n\r\n'
 
-
 def parse_request(message):
-    """Parse request takes in the incoming message from client and examines the request."""
+    """Parse request takes in the incoming message for the client"""
+    print(message)
     if message[:4] == 'GET 'or message[:7] == 'DELETE ' or message[:5] == 'POST 'or message[:4] == 'PUT ':
         if message[:4] != 'GET ':
             return response_error(405)
     else:
         return response_error(400)
-    # if message[4:15] != '/server.py ':
-    #     return response_error(400)
-    if 'HTTP/1.1' not in message[15:26] or not (message[15:26].endswith('\r\n ') or message[15:28].endswith('\\r\\n ')):
+    if 'HTTP/1.1' not in resolve_uri(message) or not (message[message.index('HTTP'):message.index('HTTP') + 11].endswith('\r\n ') or message[message.index('HTTP'):message.index('HTTP') + 13].endswith('\\r\\n ')):
+        print(message[message.index('HTTP'):message.index('HTTP') + 11] + 'HI ALLL')
         return response_error(505)
-    if 'Host' not in message[26:] or not ( message.endswith('\r\n\r\n') or message.endswith('\\r\\n\\r\\n')):
+    if 'Host' not in message[message.index('HTTP') + 11:] or not ( message.endswith('\r\n\r\n') or message.endswith('\\r\\n\\r\\n')):
         return response_error(400)
     return response_ok()
+    
 
 
 try:
@@ -57,6 +64,7 @@ try:
     server.bind(address)
     print('Server is running.')
     while True:
+        print('START')
         server.listen(1)
         conn, addr = server.accept()
         buffer_length = 8
@@ -64,18 +72,27 @@ try:
         message = ''
         flag = 1
         i = 0
-        while flag == 1:
-            iterations = math.ceil(int(conn.recv(buffer_length).decode('utf8')) / buffer_length)
-            conn.send('ok'.encode('utf8'))
+        bitmessage = b''
+        while  not message.endswith('\r\n\r\n') or  not part.endswith(b'\r\n\r\n'):
             flag = 0
-        for i in range(iterations):
             part = conn.recv(buffer_length)
+            bitmessage += part
             message += part.decode('utf8')
-        #conn.sendall(message.encode('utf8'))
-        conn.send(parse_request(message))
-        message = ''
-        #conn.send(response_ok())
+        #print(message)
+        print('here')
+        t = parse_request(message)
+        #print(t)
         conn.close()
+        break
+        if b'200' in t:
+            print('hIAALL')
+            path = resolve_uri(message)
+            if path:
+                conn.send(response_ok(path))
+            else:
+                conn.send(parse_request(message))
+                break
+        #conn.send(response_ok())
 
 except KeyboardInterrupt:
     server.close()
