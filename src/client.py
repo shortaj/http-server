a42e46446b3a3345f0d7aff011cf7d26dfec8570
Alex <ajshort2010@hotmail.com>
2017-06-01 14:53:20 -0700

    adjusted for safer code.

--- src/client.py
+++ src/client.py
@@ -3,11 +3,11 @@ import sys
 import socket
 
 
-def talk(message):
-    """."""
-    infos = socket.getaddrinfo('127.0.0.1', 5001)
+def talk(message, port=5001):
+    """The talk function takes a message and port but will default for port."""
+    infos = socket.getaddrinfo('127.0.0.1', port)
     len(infos)
-    
+
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
@@ -18,12 +18,18 @@ def talk(message):
     while not reply:
         part = client.recv(buffer_length)
         mess = mess + part.decode('utf8')
-        
-        if len(part) < buffer_length or mess == message :
+        if len(part) < buffer_length:
             break
     client.close()
     return mess
 
 
 if __name__ == '__main__':
-    print(talk(sys.argv[1]))
+    if len(sys.argv) == 2:
+        print(talk(sys.argv[1]))
+    elif len(sys.argv) == 3:
+        try:
+
+            print(talk(sys.argv[1], int(sys.argv[2])))
+        except (ConnectionRefusedError, ValueError):
+            print('Please input a valid Port.')
